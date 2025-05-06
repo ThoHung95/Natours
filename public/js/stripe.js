82b131d607d66415cd53fc26b65bc7d5a8d178ca
ThoHung95 <ntlhungnuce@gmail.com>
2025-05-06 23:02:29 +0700

    minor update

--- public/js/stripe.js
+++ public/js/stripe.js
@@ -1,5 +1,8 @@
 import axios from 'axios';
 import { showAlert } from './alerts';
+
+const apiUrl = '/api/v1/users';
+
 const { default: Stripe } = require('stripe');
 
 const stripe = Stripe(
@@ -10,7 +13,7 @@ export const bookTour = async (tourId) => {
   try {
     // 1) Get the checkout session from API:
     const session = await axios(
-      `http://127.0.0.1:3000/api/v1/bookings/checkout-session/${tourId}`,
+      `${apiUrl}/api/v1/bookings/checkout-session/${tourId}`,
     );
     // console.log(session);
     // 2) Create checkout form + charge credit card
--- public/js/updateSettings.js
+++ public/js/updateSettings.js
@@ -2,13 +2,15 @@
 import axios from 'axios';
 import { showAlert } from './alerts';
 
+const apiUrl = '/api/v1/users';
+
 // type is either 'password' or 'data'
 export const updateSettings = async (data, type) => {
   try {
     const url =
       type === 'password'
-        ? 'http://127.0.0.1:3000/api/v1/users/updateMyPassword'
-        : 'http://127.0.0.1:3000/api/v1/users/updateMe';
+        ? `${apiUrl}/api/v1/users/updateMyPassword`
+        : `${apiUrl}/api/v1/users/updateMe`;
 
     const res = await axios({
       method: 'PATCH',
